63d48a1cd2ebeb7ed9dc1381a1ceb39f216a7637
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2023-12-12 00:14:36 +0600

    Re-Code Shortest Paths Algos

---
+++ algorithms/graph-theory/Shortest Paths/bellman-ford.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const int N = 1005;
+const ll inf = 1e18;
+vector<pair<int, int>> g[N];
+vector<ll> dis(N, inf);
+int n, m;
+bool cycle;
+
+void bellman_ford(int src) {
+  dis[src] = 0;
+  for (int i = 1; i <= n; i++) {
+    for (int u = 1; u <= n; u++) {
+      for (auto [v, w] : g[u]) {
+        if (dis[v] > dis[u] + w and dis[u] != inf) {
+          if (i == n) cycle = true;
+          dis[v] = dis[u] + w;
+        }
+      }
+    }
+  }
+}
+
+int main() {
+  cin >> n >> m;
+  while (m--) {
+    int u, v, w; cin >> u >> v >> w;
+    g[u].push_back({ v,w });
+    g[v].push_back({ u,w });
+  }
+
+  cycle = false;
+  bellman_ford(1);
+
+  for (int u = 1; u <= n; u++) {
+    cout << dis[u] << ' ';
+  }
+  cout << '\n';
+
+
+  return 0;
+}
--- algorithms/graph-theory/Shortest Paths/dijkstra.cpp
+++ algorithms/graph-theory/Shortest Paths/dijkstra.cpp
@@ -1,29 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5 + 9, inf = 1e9;
+using ll = long long;
+const int N = 1e5 + 9;
+const ll inf = 1e18;
 vector<pair<int, int>> g[N];
 vector<bool> vis(N, false);
-int dis[N];
-
+vector<ll> dis(N, inf);
 int n, m;
 
 void dijkstra(int u) {
-  for (int i = 1; i <= n; i++) {
-    dis[i] = inf;
-  }
   dis[u] = 0;
-  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+  priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
   pq.push({ 0, u });
   while (!pq.empty()) {
     int selected_node = pq.top().second;
-    int d = pq.top().first;
+    ll d = pq.top().first;
     pq.pop();
     if (vis[selected_node]) continue;
     vis[selected_node] = true;
     for (auto [v, w] : g[selected_node]) {
-      if (dis[v] > (d + w)) {
-        dis[v] = d + w;
+      if (dis[v] > (1ll * d + w)) {
+        dis[v] = 1ll * d + w;
         pq.push({ dis[v], v });
       }
     }
@@ -35,14 +33,15 @@ int main() {
   while (m--) {
     int u, v, w; cin >> u >> v >> w;
     g[u].push_back({ v,w });
-    g[v].push_back({ u,w });
   }
 
   dijkstra(1);
 
   for (int u = 1; u <= n; u++) {
-    cout << u << ": " << dis[u] << '\n';
+    cout << dis[u] << ' ';
   }
+  cout << '\n';
 
   return 0;
 }
+// https://cses.fi/problemset/task/1671/
